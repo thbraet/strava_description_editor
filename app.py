e59d36ebf04b01443760acbe67968a13ff22b5f6
Thibauld Braet <thibauld@live.be>
2024-05-26 08:52:15 +0200

    Create placeholder for Strava landing page:

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def about():
 def show_user_profile(username):
     return f"User {username}"
 
+@app.route('/strava')
+def strava():
+    return render_template('strava.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
