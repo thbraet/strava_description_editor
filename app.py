0b7dc787cb7e833cbbb679c23d2f8b28a32c4407
Thibauld Braet <thibauld@live.be>
2024-05-26 08:45:24 +0200

    Add initial routing functionalities

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route('/about')
+def about():
+    return "This is the about page."
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f"User {username}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
